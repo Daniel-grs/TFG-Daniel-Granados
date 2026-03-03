c9dfeb5bec5d73821b86f70a3529da1b0c5eaa83
Daniel-grs <danielgranadossolves@gmail.com>
2026-03-03 11:11:59 +0100

    Initial commit

---
+++ src/main/java/es/metrica/sept25/evolutivo/service/weather/WeatherService.java
@@ -0,0 +1,9 @@
+package es.metrica.sept25.evolutivo.service.weather;
+
+import java.util.Optional;
+
+import es.metrica.sept25.evolutivo.domain.dto.weather.Weather;
+
+public interface WeatherService {
+	public Optional<Weather> getWeather(String code);
+}
